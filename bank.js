33b647a2823bfc350c39a578706e4714855db445
saurav <sauravpaljou@gmail.com>
2024-04-17 10:22:09 +0600

    init

---
+++ bank.js
@@ -0,0 +1,12 @@
+// Define the printLanguages function
+function printLanguages(languages) {
+    for (let language of languages) {
+        console.log(language);
+    }
+}
+
+// The array of languages
+const languagesArray = ['PHP', 'JavaScript', 'Python'];
+
+// Call the printLanguages function with the array (languagesArray) as argument
+printLanguages(languagesArray);
